d6a86e44a0642bfff0076bbdd94a80ce78476950
spvagelis <spirou.vagelis@gmail.com>
2025-01-24 18:29:28 +0200

    Update AntifraudSDK from 1.0.3 to 1.0.4 Update sharedSPM from 1.0.2 to 1.0.3

--- Package.swift
+++ Package.swift
@@ -1,6 +1,41 @@
 // swift-tools-version: 5.10
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
+//import PackageDescription
+//
+//let package = Package(
+//    name: "AntifraudSPM",
+//    platforms: [.iOS(.v15)],
+//    products: [
+//        // Products define the executables and libraries a package produces, making them visible to other packages.
+//        .library(
+//            name: "AntifraudSPM",
+//            type: .dynamic,
+//            targets: ["AntifraudSPM"]),
+//    ],
+//    dependencies: [
+//        .package(url: "https://github.com/bespot/sharedSPM.git", exact: "1.0.2")
+//    ],
+//    targets: [
+//        // Targets are the basic building blocks of a package, defining a module or a test suite.
+//        // Targets can depend on other targets in this package and products from dependencies.
+//        .target(
+//            name: "AntifraudSPM",
+//            dependencies: [
+//                .target(name: "AntifraudSDK"),
+//                .product(name: "sharedSPM", package: "sharedSPM")
+//            ]
+//        ),
+//        .testTarget(
+//            name: "AntifraudSPMTests",
+//            dependencies: ["AntifraudSPM"]),
+//        .binaryTarget(
+//            name: "AntifraudSDK",
+//            url: "https://github.com/bespot/antifraud-sdk-ios-release/releases/download/1.0.3/AntifraudSDK.xcframework.zip",
+//            checksum: "8ed590973e38f04a7499e1921ea8dea2983fe847152d90ecacab4a8cc105adae")
+//    ]
+//)
+
 import PackageDescription
 
 let package = Package(
@@ -14,7 +49,7 @@ let package = Package(
             targets: ["AntifraudSPM"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/bespot/sharedSPM.git", exact: "1.0.2")
+        .package(url: "https://github.com/bespot/sharedSPM.git", exact: "1.0.3")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
@@ -31,7 +66,7 @@ let package = Package(
             dependencies: ["AntifraudSPM"]),
         .binaryTarget(
             name: "AntifraudSDK",
-            url: "https://github.com/bespot/antifraud-sdk-ios-release/releases/download/1.0.3/AntifraudSDK.xcframework.zip",
-            checksum: "8ed590973e38f04a7499e1921ea8dea2983fe847152d90ecacab4a8cc105adae")
+            url: "https://github.com/bespot/antifraud-sdk-ios-release/releases/download/1.0.4/AntifraudSDK.xcframework.zip",
+            checksum: "29bc45e8ec60530072b576f037fe7e0a7cb7f2494ea001ac737e263195ab6b7b")
     ]
 )
