9443cd166981a8f6f34bc0b01abfb2579f1c0e83
spvagelis <spirou.vagelis@gmail.com>
2025-01-27 13:57:29 +0200

    Update Package.swift file for 1.0.9 release

--- Package.swift
+++ Package.swift
@@ -32,7 +32,7 @@ let package = Package(
             dependencies: ["AntifraudSPM"]),
         .binaryTarget(
             name: "AntifraudSDK",
-            url: "https://github.com/bespot/antifraud-sdk-ios-release/releases/download/1.0.8/AntifraudSDK.xcframework.zip",
+            url: "https://github.com/bespot/antifraud-sdk-ios-release/releases/download/1.0.9/AntifraudSDK.xcframework.zip",
             checksum: "71bbfcc33db72bf230bb1edbcfaa5e6a230a4ea4cd5586dff909eb81a726176e")
     ]
 )
