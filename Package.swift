621763d4159c0adfeca604b0a45d53c6bd94b19f
spvagelis <spirou.vagelis@gmail.com>
2025-05-28 16:57:55 +0300

    - Introduces `subscribe` and `unsubscribe` methods to allow clients to observe real-time fraud detection updates and stop receiving them, respectively. - Updates the AntifraudSDK version to 1.1.2. - updated AntifraudSDK.podspec - Updated CHANGELOG.md - Updated Package.swift - Updated README.md - Updated the AntifraudSDK.xcframework for version 1.1.2

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "AntifraudSDK",
-            url: "https://github.com/bespot/antifraud-sdk-ios-release/releases/download/1.1.1/AntifraudSDK.xcframework.zip",
-            checksum: "bd45a6e00cf340dfc541aff3725b52eea9784309ae9fc3464519381887572c28"
+            url: "https://github.com/bespot/antifraud-sdk-ios-release/releases/download/1.1.2-test/AntifraudSDK.xcframework.zip",
+            checksum: "bd7df855dc8f274ecfb2f30004ff390895c75a10874213d5b9fbbafb2a02c8c2"
         )
     ]
 )
